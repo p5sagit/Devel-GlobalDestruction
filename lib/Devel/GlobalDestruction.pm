19a66cb4bd2afb3036d4ad05c6e30713023bc107
Peter Rabbitson <ribasushi@cpan.org>
2012-06-14 00:56:26 +0200

    Version 0.06

--- lib/Devel/GlobalDestruction.pm
+++ lib/Devel/GlobalDestruction.pm
@@ -3,7 +3,7 @@ package Devel::GlobalDestruction;
 use strict;
 use warnings;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use Sub::Exporter -setup => {
     exports => [ qw(in_global_destruction) ],
