78be2b4a5f7971e996f688e20ae18d3e7e4931db
Florian Ragwitz <rafl@debian.org>
2012-07-25 11:14:16 -0400

    Version 0.07

--- lib/Devel/GlobalDestruction.pm
+++ lib/Devel/GlobalDestruction.pm
@@ -3,7 +3,7 @@ package Devel::GlobalDestruction;
 use strict;
 use warnings;
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 use Sub::Exporter -setup => {
     exports => [ qw(in_global_destruction) ],
