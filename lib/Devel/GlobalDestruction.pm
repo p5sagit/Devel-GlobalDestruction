1c89491db81832310d3dfa9037a0fea4d4acd98e
Florian Ragwitz <rafl@debian.org>
2012-07-31 16:19:31 -0400

    Version 0.08

--- lib/Devel/GlobalDestruction.pm
+++ lib/Devel/GlobalDestruction.pm
@@ -3,7 +3,7 @@ package Devel::GlobalDestruction;
 use strict;
 use warnings;
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 use Sub::Exporter::Progressive -setup => {
     exports => [ qw(in_global_destruction) ],
