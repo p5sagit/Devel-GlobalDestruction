9e92a6224ec77eca7deca4c700cf76ffd3b8b061
Graham Knop <haarg@haarg.org>
2013-04-03 11:50:49 -0400

    Version 0.11

--- lib/Devel/GlobalDestruction.pm
+++ lib/Devel/GlobalDestruction.pm
@@ -3,7 +3,7 @@ package Devel::GlobalDestruction;
 use strict;
 use warnings;
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 use Sub::Exporter::Progressive -setup => {
   exports => [ qw(in_global_destruction) ],
